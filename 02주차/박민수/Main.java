6c4ebbe65b5f3349d4408162096e85d903388b96
pmsu2007 <pmsu2008@gmail.com>
2023-10-16 23:07:59 +0900

    13549 : 숨바꼭질 3

--- 02주차/박민수/Main.java
+++ 02주차/박민수/Main.java
@@ -3,6 +3,7 @@ package BFS.BOJ13549;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -21,9 +22,10 @@ public class Main {
 
     static int N;
     static int K;
-    static final int MAX = 100000;
-    static boolean[] visited = new boolean[100001];
-    static Queue<Distance> q = new LinkedList<>();
+    static final int DISTANCE_MAX = 100000;
+    static int[] counter = new int[100001];
+
+    static Queue<Integer> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,38 +33,41 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+        Arrays.fill(counter, Integer.MAX_VALUE);
 
         // 처음 위치 큐에 삽입
-        q.add(new Distance(N, 0));
-        visited[N] = true;
+        q.add(N);
+        counter[N] = 0;
         while(!q.isEmpty()) {
-            Distance now = q.poll();
+            int now = q.poll();
 
-            if (now.x == K) {
-                System.out.println(now.time);
+            if (now == K) {
+                System.out.println(counter[K]);
                 break;
             }
 
             // 걷는 경우, 순간이동 하는 경우 계산
             for (int i = 0; i < 3; i++) {
-                Distance next = null;
 
                 switch (i) {
                     case 0:
-                        next = new Distance(now.x - 1, now.time + 1);
+                        if (now - 1 >= 0 && counter[now - 1] > counter[now] + 1) {
+                            q.add(now - 1);
+                            counter[now - 1] = counter[now] + 1;
+                        }
                         break;
                     case 1:
-                        if (now.x + 1 <= MAX)
-                            next = new Distance(now.x + 1, now.time + 1);
+                        if (now + 1 <= DISTANCE_MAX && counter[now + 1] > counter[now] + 1) {
+                            q.add(now + 1);
+                            counter[now + 1] = counter[now] + 1;
+                        }
                         break;
                     case 2:
-                        if (now.x * 2 <= MAX)
-                            next = new Distance(now.x * 2, now.time);
+                        if (now * 2 <= DISTANCE_MAX && counter[now * 2] > counter[now]) {
+                            q.add(now * 2);
+                            counter[now * 2] = counter[now];
+                        }
                         break;
-                }
-
-                if (next != null && !visited[next.x]) {
-                    q.add(next);
                 }
             }
         }
